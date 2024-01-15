7760eef31991b6a0383702d649995d2f8b4ce6d0
larswmh <lars.wittmershaus@cuisine.digital>
2024-01-15 13:08:35 +0100

    Rearrange settings

--- src/components/settings/General.vue
+++ src/components/settings/General.vue
@@ -1,40 +1,48 @@
 <template>
     <div>
         <form class="my-4" autocomplete="off" @submit.prevent="saveGeneral">
-            <!-- Client side Timezone -->
+            <!-- Primary Base URL -->
             <div class="mb-4">
-                <label for="timezone" class="form-label">
-                    {{ $t("Display Timezone") }}
+                <label class="form-label" for="primaryBaseURL">
+                    {{ $t("Primary Base URL") }}
                 </label>
-                <select id="timezone" v-model="$root.userTimezone" class="form-select">
-                    <option value="auto">
-                        {{ $t("Auto") }}: {{ guessTimezone }}
-                    </option>
-                    <option
-                        v-for="(timezone, index) in timezoneList"
-                        :key="index"
-                        :value="timezone.value"
-                    >
-                        {{ timezone.name }}
-                    </option>
-                </select>
+
+                <div class="input-group mb-3">
+                    <input
+                        id="primaryBaseURL"
+                        v-model="settings.primaryBaseURL"
+                        class="form-control"
+                        name="primaryBaseURL"
+                        placeholder="https://"
+                        pattern="https?://.+"
+                        autocomplete="new-password"
+                    />
+                    <button class="btn btn-outline-primary" type="button" @click="autoGetPrimaryBaseURL">
+                        {{ $t("Auto Get") }}
+                    </button>
+                </div>
+
+                <div class="form-text"></div>
             </div>
 
-            <!-- Server Timezone -->
+            <!-- DevOps project directory -->
             <div class="mb-4">
-                <label for="timezone" class="form-label">
-                    {{ $t("Server Timezone") }}
+                <label class="form-label" for="devopsDirectory">
+                    {{ $t("DevOps project directory") }}
                 </label>
-                <select id="timezone" v-model="settings.serverTimezone" class="form-select">
-                    <option value="UTC">UTC</option>
-                    <option
-                        v-for="(timezone, index) in timezoneList"
-                        :key="index"
-                        :value="timezone.value"
-                    >
-                        {{ timezone.name }}
-                    </option>
-                </select>
+
+                <div class="input-group mb-3">
+                    <input
+                        id="devopsDirectory"
+                        v-model="settings.devopsDirectory"
+                        class="form-control"
+                        name="devopsDirectory"
+                        placeholder="org/project"
+                        pattern="[a-zA-Z0-9-]+\/[a-zA-Z0-9-]+"
+                        autocomplete="new-password"
+                    />
+                </div>
+                <div class="form-text">If specified, Teams notifications will include buttons to create a ticket with the notification information, or to search for existing tickets in that directory.</div>
             </div>
 
             <!-- Search Engine -->
@@ -108,50 +116,6 @@
                 </div>
             </div>
 
-            <!-- Primary Base URL -->
-            <div class="mb-4">
-                <label class="form-label" for="primaryBaseURL">
-                    {{ $t("Primary Base URL") }}
-                </label>
-
-                <div class="input-group mb-3">
-                    <input
-                        id="primaryBaseURL"
-                        v-model="settings.primaryBaseURL"
-                        class="form-control"
-                        name="primaryBaseURL"
-                        placeholder="https://"
-                        pattern="https?://.+"
-                        autocomplete="new-password"
-                    />
-                    <button class="btn btn-outline-primary" type="button" @click="autoGetPrimaryBaseURL">
-                        {{ $t("Auto Get") }}
-                    </button>
-                </div>
-
-                <div class="form-text"></div>
-            </div>
-
-            <!-- DevOps project directory -->
-            <div class="mb-4">
-                <label class="form-label" for="devopsDirectory">
-                    {{ $t("DevOps project directory") }}
-                </label>
-
-                <div class="input-group mb-3">
-                    <input
-                        id="devopsDirectory"
-                        v-model="settings.devopsDirectory"
-                        class="form-control"
-                        name="devopsDirectory"
-                        placeholder="org/project"
-                        pattern="[a-zA-Z0-9-]+\/[a-zA-Z0-9-]+"
-                        autocomplete="new-password"
-                    />
-                </div>
-                <div class="form-text">For interactive buttons on the Teams notification, allowing you to create a Ticket directly from Teams or look for existing tickets.</div>
-            </div>
-
             <!-- DNS Cache (nscd) -->
             <div v-if="$root.info.isContainer" class="mb-4">
                 <label class="form-label">
@@ -211,6 +175,42 @@
                 <div class="form-text">
                     {{ $t("chromeExecutableDescription") }}
                 </div>
+            </div>
+
+            <!-- Client side Timezone -->
+            <div class="mb-4">
+                <label for="timezone" class="form-label">
+                    {{ $t("Display Timezone") }}
+                </label>
+                <select id="timezone" v-model="$root.userTimezone" class="form-select">
+                    <option value="auto">
+                        {{ $t("Auto") }}: {{ guessTimezone }}
+                    </option>
+                    <option
+                        v-for="(timezone, index) in timezoneList"
+                        :key="index"
+                        :value="timezone.value"
+                    >
+                        {{ timezone.name }}
+                    </option>
+                </select>
+            </div>
+
+            <!-- Server Timezone -->
+            <div class="mb-4">
+                <label for="timezone" class="form-label">
+                    {{ $t("Server Timezone") }}
+                </label>
+                <select id="timezone" v-model="settings.serverTimezone" class="form-select">
+                    <option value="UTC">UTC</option>
+                    <option
+                        v-for="(timezone, index) in timezoneList"
+                        :key="index"
+                        :value="timezone.value"
+                    >
+                        {{ timezone.name }}
+                    </option>
+                </select>
             </div>
 
             <!-- Save Button -->
