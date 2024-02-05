72bf10d5ff16038efbebec1a4dbb61188dccc3c7
larswmh <lars.wittmershaus@cuisine.digital>
2024-02-05 09:40:37 +0100

    Pass lint test 2

--- src/pages/Pipelines.vue
+++ src/pages/Pipelines.vue
@@ -14,8 +14,7 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="(pipeline, index) in pipelines.slice().reverse()" :key="index"
-                                :class="{ 'shadow-box': $root.windowWidth <= 550 }">
+                            <tr v-for="(pipeline, index) in pipelines.slice().reverse()" :key="index" :class="{ 'shadow-box': $root.windowWidth <= 550 }">
                                 <td><a :href="pipeline._url" target="_blank">{{ pipeline._project }}</a></td>
                                 <td>
                                     <Status :status="pipeline._status" />
